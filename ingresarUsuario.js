b9ae91391d7967a6f6b56a537d01918dc7fd9e21
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com>
2024-04-21 00:22:47 -0500

    ingresarUsuario.js 1.0

---
+++ ingresarUsuario.js
@@ -0,0 +1,30 @@
+window.onload=inicio 
+function inicio() {
+    document.querySelector("#botonIngresar").onclick=inicioCARRITOCIBER
+    document.querySelector(".caja-nombre").onkeydown=tecladoInicioCARRITOCIBER
+    document.querySelector(".caja-contraseña").onkeydown=tecladoInicioCARRITOCIBER
+}
+function inicioCARRITOCIBER() {
+    let nombre=document.querySelectorAll(".inputInicio")[0].value
+    let contraseña=document.querySelectorAll(".inputInicio")[1].value
+    console.log(contraseña)
+
+    if (nombre=="leonel" && contraseña=="123"|| nombre=="leo" && contraseña=="123"|| nombre=="LEONEL" && contraseña=="123"|| nombre=="Leonel" && contraseña=="123") {
+        location.href="index.html"    
+    }
+    
+}
+
+function tecladoInicioCARRITOCIBER(e) {
+    let codigo_tecla = e.keyCode
+    if (codigo_tecla==13) {
+
+        inicioCARRITOCIBER()
+    }
+    if (codigo_tecla==37) {
+        location.href="ingresarUsuario.html"
+        
+    }
+    
+}
+
