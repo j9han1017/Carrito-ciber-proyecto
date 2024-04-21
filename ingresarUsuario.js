f2113fee789f5556200b9e0e8d03348538571fd5
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com>
2024-04-21 14:30:44 -0500

    MEJORARA DE ingresousuario.js

--- ingresarUsuario.js
+++ ingresarUsuario.js
@@ -1,20 +1,30 @@
 window.onload=inicio 
+
+    let cuentas=[
+        {nombre:"leonel",contraseña:"123"},
+        {nombre:"diaz",contraseña:"123"}
+    ]
+
 function inicio() {
     document.querySelector("#botonIngresar").onclick=inicioCARRITOCIBER
     document.querySelector(".caja-nombre").onkeydown=tecladoInicioCARRITOCIBER
     document.querySelector(".caja-contraseña").onkeydown=tecladoInicioCARRITOCIBER
 }
 function inicioCARRITOCIBER() {
-    let nombre=document.querySelectorAll(".inputInicio")[0].value
-    let contraseña=document.querySelectorAll(".inputInicio")[1].value
-    console.log(contraseña)
+    let nombreInput=document.querySelectorAll(".inputInicio")[0].value.toLowerCase()
+    let contraseñaInput=document.querySelectorAll(".inputInicio")[1].value
 
-    if (nombre=="leonel" && contraseña=="123"|| nombre=="leo" && contraseña=="123"|| nombre=="LEONEL" && contraseña=="123"|| nombre=="Leonel" && contraseña=="123") {
-        location.href="index.html"    
-    }
-    if (nombre=="diaz" && contraseña=="123"|| nombre=="Diaz" && contraseña=="123"|| nombre=="DIAZ" && contraseña=="123"|| nombre=="diaZ" && contraseña=="123") {
-        location.href="index.html"    
-    }
+
+for (let k = 0; k < cuentas.length; k++) {
+  if (cuentas[k].nombre==nombreInput && cuentas[k].contraseña==contraseñaInput) {
+    alert("soy " +cuentas[k].nombre )
+    location.href="index.html"  
+  }else{
+    alert("crea una cuenta")
+  }
+    
+}
+
 }
 
 function tecladoInicioCARRITOCIBER(e) {
